9199d47524b5b98fde3f4f7aecfb86c1b356f526
Adam Campbell <adamcampbell@adams-mbp.home>
2017-09-21 22:40:58 -0500

    updates

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+    BASE_API_URL = 'http://api.nwfsports.com.adam.webdev.thecodify.com';
+}
